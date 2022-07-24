0a2ecd3db57d9843ca4fd12828477c24bc5c7c7d
adrian <adrianafnandika@gmail.com>
2022-07-24 12:05:09 +0700

    sepatare

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World aa aa  aaaa!"
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+def home_view():
+        return "<h1>Hello World!</h1>"
